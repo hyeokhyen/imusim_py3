7988874fc2701d30f5ad5ec458542bc5f1f9878c
hyeok.kwon <hyeokhyen@gmail.com>
2021-04-09 00:44:49 +0000

    failed offsetting rotation and position

--- demo_local_coord.py
+++ demo_local_coord.py
@@ -1,9 +1,15 @@
+'''
+Global coordinate is identity(3)
+The person is flipped in z axis
+Need to implement IdealIMUoffset -> demo_offset.py
+'''
+
 from imusim.platforms.imus import IdealIMU
 from imusim.io.bvh import BVHLoader
 from imusim.simulation.base import Simulation
 from imusim.behaviours.imu import BasicIMUBehaviour
 from imusim.trajectories.rigid_body import SplinedBodyModel
-
+from pprint import pprint
 import numpy as np
 
 file_bvh = './141_01.bvh'
@@ -15,9 +21,34 @@ with open(file_bvh, 'r') as bvhFile:
 	loader._readMotionData()
 	model = loader.model
 
+list_joint = []
+list_bone = {}
+list_position = {}
+# for p in model.points:
+# 	print (p.name)
+# assert False
+
+for p in model.joints:
+	print (p.name)
+	list_joint.append(p.name)
+	list_bone[p.name] = []
+
+	for child in p.children:
+		list_bone[p.name].append(child.name)
+		print (child.name)
+	print ('-----------')
+pprint (list_bone)
+# assert False
+
 splinedModel = SplinedBodyModel(model)
 startTime = splinedModel.startTime
 endTime = splinedModel.endTime
+
+print (model.positionKeyFrames.timestamps)
+
+joint = splinedModel.getJoint('Hips')
+print (joint.position(startTime))
+assert False
 
 samplingPeriod = (endTime - startTime)/loader.frameCount
 
@@ -25,6 +56,10 @@ sim = Simulation()
 sim.time = startTime
 
 imu = IdealIMU()
+
+rotationOffset = imu.accelerometer._rotationOffset
+# print (rotationOffset.toMatrix())
+# assert False
 # print(imu.accelerometer._positionOffset)
 # print(imu.accelerometer._rotationOffset)
 # print(imu.gyroscope._positionOffset)
@@ -44,14 +79,45 @@ sim.run(endTime)
 # print(imu.gyroscope._rotationOffset)
 
 timestamps = imu.accelerometer.rawMeasurements.timestamps
-print (timestamps.shape)
+# print (timestamps.shape)
 
-print ('trajectory')
+acc = np.empty((timestamps.shape[0], 3))
+acc_volt = np.empty((timestamps.shape[0], 3))
+acc_rotFrame = np.empty((timestamps.shape[0], 3))
 acc_position = np.empty((timestamps.shape[0], 3))
-acc_rotation = np.empty((timestamps.shape[0], 3))
+acc_rotation = np.empty((timestamps.shape[0], 3, 3))
+
+gyro = np.empty((timestamps.shape[0], 3))
 gyro_position = np.empty((timestamps.shape[0], 3))
-gyro_rotation = np.empty((timestamps.shape[0], 3))
+gyro_rotation = np.empty((timestamps.shape[0], 3, 3))
+
+gravity_pos = imu.accelerometer.platform.simulation.environment.gravitationalField
+gravity = np.empty((timestamps.shape[0], 3))
+
 for i, t in enumerate(timestamps):
+	# gravity
+	g = gravity_pos(imu.accelerometer.trajectory.position(t), t)
+	# print (g)
+	gravity[i] = g.reshape((-1,))
+	
+	# acceleration
+	_acc = imu.accelerometer.trajectory.acceleration(t)
+	# print (_acc)
+	# assert False
+	acc[i] = _acc.reshape((-1,))
+
+	# acceleration - gravity -> rotateFrame
+	a = imu.accelerometer.trajectory.rotation(t).rotateFrame(_acc - g)
+	acc_rotFrame[i] = a.reshape((-1,))
+
+	# voltage
+	volt = imu.accelerometer.voltages(t)
+	acc_volt[i] = volt.reshape((-1,))
+	# print (volt)
+	# print (_acc)
+	# print ('----------')
+	# assert False
+
 	# position
 	acc_pose = imu.accelerometer.trajectory.position(t)
 	acc_position[i] = acc_pose.reshape((-1,))
@@ -61,14 +127,23 @@ for i, t in enumerate(timestamps):
 	# print (acc_pose-gyro_pose)
 
 	# rotation
+	model_rot = splinedModel.getJoint('Hips').rotation(t).toMatrix()
+	# print (model_rot)	
+	
 	acc_quat = imu.accelerometer.trajectory.rotation(t)
-	acc_rot.to
-	print (acc_quat)
-	acc_rotation[i] = acc_quat.reshape((-1,))
+	acc_rot = acc_quat.toMatrix()
+	acc_rotation[i] = acc_rot
+
+	_gyro = imu.accelerometer.trajectory.rotationalVelocity(t)
+	gyro[i] = _gyro.reshape((-1,))
 
 	gyro_quat = imu.gyroscope.trajectory.rotation(t)
-	gyro_rotation[i] = gyro_quat.reshape((-1,))   
-	print (acc_rot-gyro_quat)
+	gyro_rot = gyro_quat.toMatrix()
+	gyro_rotation[i] = gyro_rot   
+	# print (acc_rot-model_rot)
+	# print (acc_rot-gyro_rot)
+	
+	# print ('-------------')
 
 # assert np.array_equal(acc_position, gyro_position)
 
@@ -77,6 +152,17 @@ for i, t in enumerate(timestamps):
 # print (gyro_trajectory)
 # print (gyro_trajectory.shape)
 
+# print (gravity)
+if 1:
+	acc_raw = imu.accelerometer.rawMeasurements.values.T
+	print (acc_raw.shape)
+	for i in range(acc.shape[0]-4):
+		# print (acc[i+4])
+		# print (acc_rotFrame[i+4])
+		print (acc_volt[i+4])
+		print (acc_raw[i])
+		print ('---------------------')
+
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -84,5 +170,22 @@ from mpl_toolkits.mplot3d import Axes3D
 import mpl_toolkits.mplot3d.axes3d as p3
 
 fig = plt.figure()
-ax = p3.Axes3D(fig)
+ax = fig.add_subplot(311)
+ax.plot(acc_rotFrame[4:,0], 'b')
+ax.plot(acc_raw[:-4,0], 'r')
+
+ax = fig.add_subplot(312)
+ax.plot(acc_rotFrame[4:,1], 'b')
+ax.plot(acc_raw[:-4,1], 'r')
+
+ax = fig.add_subplot(313)
+ax.plot(acc_rotFrame[4:,2], 'b')
+ax.plot(acc_raw[:-4,2], 'r')
+
+fig.tight_layout()
+
+file_fig = '/home/ubuntu/dataset/analysis/imusim_py3/acc.png'
+fig.savefig(file_fig)
+print ('save in ...', file_fig)
+
 
